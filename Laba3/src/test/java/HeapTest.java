7730221fe42f520ba10c31d5ae715e8f93d85f43
RogeVestnazayr <31630996+RogeVestnazayr@users.noreply.github.com>
2017-12-09 15:05:43 +0200

    Test Changes

--- Laba3/src/test/java/HeapTest.java
+++ Laba3/src/test/java/HeapTest.java
@@ -2,19 +2,19 @@
  * Created by Egor on 09.12.2017.
  */
     import ArrayClasses.HeapSort;
-    import org.junit.Test;
+            import org.junit.Test;
 
-import static org.junit.Assert.*;
+            import static org.junit.Assert.*;
 
-    public class HeapTest {
-        HeapSort heapClass = new HeapSort();
-        @Test
-        public void heapSort() {
-            int[] testArray = {3,6,1,3,2};
-            int[] sortedArray = {1,2,3,3,6};
-            heapClass.sort(testArray);
-            assertArrayEquals(sortedArray,testArray );
-        }
-
+public class HeapTest {
+    HeapSort heapClass = new HeapSort();
+    @Test
+    public void heapSort() {
+        int[] testArray = {3,6,1,3,2};
+        int[] sortedArray = {1,2,3,3,6};
+        heapClass.sort(testArray);
+        assertArrayEquals(sortedArray,testArray );
     }
+
+}
 
